7115f61af426fa1c1ba9ba3dc1d8ff8ea0f5324b
Christopher Wilding <wilding.christopher@gmail.com>
2014-05-08 23:10:32 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='sahi',
+    version='0.1.0',
+    packages=['sahi', 'sahi.test'],
+    url='',
+    license='Apache 2.0',
+    author='Christopher Wilding',
+    author_email='wilding.christopher@gmail.com',
+    description='Python driver for Sahi'
+)
